c834515cc160d581dfc62f1f9b819b50e44f8842
cawatou <cawatou@gmail.com>
2017-08-08 13:41:55 +0300

    init

---
+++ customtours.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once('SmartyBC.class.php');
+$smarty = new SmartyBC();
+
+$smarty->setTemplateDir('customtemplates');
+
+
+echo 'hi all';
+
+
+
+    // $dbHost = "localhost";
+    // $dbUser = "neoz";
+    // $dbPass = "J0p9P0n9";
+    // $dbName = "tours";
+
+
+    // $myConnect = mysql_connect($dbHost,$dbUser,$dbPass));
+    // mysql_select_db($dbName,$myConnect);
+
+
+    // //$qwer=mysql_query("SELECT UF_MIN_PRICE FROM  `b_uts_iblock_20_section` WHERE  `VALUE_ID` =5292;",$myConnect);
+
+
+    // $qwer=mysql_query("SELECT * FROM  `b_iblock_section` WHERE  `IBLOCK_ID` =20;",$myConnect);
+
+    
+
+    // while ($arr=mysql_fetch_array($qwer))
+    // {
+    //     print_r($arr);
+    // }
+
+
+
+    // mysql_close($myConnect);
+
+
